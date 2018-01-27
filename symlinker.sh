7892f41ff52983529e05d1932d8bd20b28295d73
Jørn Iversen Efteland <jorn@jorn.j>
2018-01-27 23:37:42 +0100

    Updated with gitconfig.

--- symlinker.sh
+++ symlinker.sh
@@ -6,3 +6,4 @@ ln -sf ${repo_name}/i3/config ~/.config/i3/config
 ln -sf ${repo_name}/i3status/config ~/.config/i3status/config
 ln -sf ${repo_name}/fish/config.fish ~/.config/fish/config.fish
 ln -sf ${repo_name}/vim/vimrc ~/.vimrc
+ln -sf ${repo_name}/gitconfig ~/.gitconfig
