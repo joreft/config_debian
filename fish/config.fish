838c09df8cb410caf550556464edfafdc5c8ee86
Jørn Efteland <joreft@gmail.com>
2018-02-17 23:20:10 +0100

    Tab completion highlighting fixed.

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,5 @@ set fish_greeting Speak\ \e\[32mfriend\e\[30m\e\(B\e\[m\ and\ enter
 set __fish_git_prompt_color_branch yellow --bold
 
 set PATH $PATH ~/.cargo/bin/
+
+set fish_color_search_match --background='#2e4b7a'
